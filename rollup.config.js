d5aabe4e91e25192f9e7afad632ffb38dcc7dd1d
raphaeljoie <9681129+raphaeljoie@users.noreply.github.com>
2022-07-01 20:19:26 +0200

    prevent bundle react

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import external from 'rollup-plugin-peer-deps-external';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
@@ -23,6 +24,7 @@ export default [
       },
     ],
     plugins: [
+      external(),
       resolve({ extensions: ['.jsx', '.js'] }),
       babel({
         exclude: 'node_modules/**',
